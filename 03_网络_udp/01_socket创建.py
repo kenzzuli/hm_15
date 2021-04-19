b55eb296d2e46ee104ac1f7c136273c9fb1f2031
kenzzuli <820710063@qq.com>
2021-04-19 13:18:05 +0800

    use socket to send msg

--- 03_网络_udp/01_socket创建.py
+++ 03_网络_udp/01_socket创建.py
@@ -6,6 +6,14 @@ def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # 可以使用套接字收发数据
+    # 接收用户的输入，并按照bytes编码
+    msg = input("please input message: ").encode(encoding="utf-8", errors="strict")
+    # 在字符串前加b，也表示按照bytes编码
+    msg2 = b"abc"
+    # 地址 (ip, port)
+    addr = "10.4.51.50", 4444
+    udp_socket.sendto(msg, addr)
+    udp_socket.sendto(msg2, addr)
 
     # 关闭套接字
     udp_socket.close()
