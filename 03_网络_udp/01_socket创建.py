0ff7ca30eaa0241579276674635a6a7a7255e22b
kenzzuli <820710063@qq.com>
2021-04-19 13:29:47 +0800

    update some comments

--- 03_网络_udp/01_socket创建.py
+++ 03_网络_udp/01_socket创建.py
@@ -6,11 +6,11 @@ def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # 可以使用套接字收发数据
-    # 接收用户的输入，并按照bytes编码
+    # 接收用户的输入，并转成bytes类型
     msg = input("please input message: ").encode(encoding="utf-8", errors="strict")
-    # 在字符串前加b，也表示按照bytes编码
+    # 在字符串前加b，也表示bytes类型
     msg2 = b"abc"
-    # 地址 (ip, port)
+    # 地址 (ip, port) 元组 ip是字符串，port是数字
     addr = "10.4.51.50", 4444
     udp_socket.sendto(msg, addr)
     udp_socket.sendto(msg2, addr)
