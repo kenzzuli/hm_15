25f4316059bc1e2084b9435d9995ba83eb8ba737
kenzzuli <820710063@qq.com>
2021-05-04 14:31:11 +0800

    全局变量作为实参传给函数，在函数内部修改形参，全局变量是否会变化

--- 05_多任务-线程/09_将全局变量以参数传递给函数.py
+++ 05_多任务-线程/09_将全局变量以参数传递给函数.py
@@ -1,27 +1,42 @@
 # 测试全局变量是否会被修改
 def test(num):
+    print(id(num))
     num += 1
+    print(id(num))  # id变化
 
 
 def test2(num_list):
-    num_list += [11]
+    print(id(num_list))
+    num_list += [11]  # 对列表使用+=操作，实质上是调用列表的extend方法！而不是使用相加再赋值的操作！
+    print(id(num_list))  # id不变
+
+
+def test4(num_list):
+    print(id(num_list))
+    num_list = [66]
+    print(id(num_list))  # id变化
 
 
 def test3(num_list):
+    print(id(num_list))
     num_list.append(111)
+    print(id(num_list))  # id不变
 
 
 num = 1
 num_list1 = [11, ]
 num_list2 = [111, ]
+num_list3 = [1111, ]
 test(num)
 test2(num_list1)
 test3(num_list2)
+test4(num_list3)
 
 print(num)
 print(num_list1)
 print(num_list2)
-
+print(num_list3)
 # 1
 # [11, 11]
 # [111, 111]
+# [1111]
