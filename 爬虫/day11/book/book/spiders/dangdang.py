2445e8b8f437a895cec0629a9de1e3edb25a2881
kenzzuli <820710063@qq.com>
2021-08-15 18:03:08 +0800

    distributed spider with redis

---
+++ 爬虫/day11/book/book/spiders/dangdang.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class DangdangSpider(scrapy.Spider):
+    name = 'dangdang'
+    allowed_domains = ['dangdang.com']
+    start_urls = ['http://dangdang.com/']
+
+    def parse(self, response):
+        pass
